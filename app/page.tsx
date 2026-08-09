35f1862ad1da26db3979282b6deacd92b12640ca
Luis Jimenez <luisj305@gmail.com>
2026-08-09 03:57:55 -0400

    Redirect homepage to login

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,5 @@
-import { supabase } from "@/lib/supabase/client";
+import { redirect } from "next/navigation";
 
-export default async function Home() {
-  const { data, error } = await supabase
-    .from("projects")
-    .select("*");
-
-  return (
-    <main style={{ padding: 40 }}>
-      <h1>SUSOTECH MVP</h1>
-
-      <pre>
-        {JSON.stringify({ data, error }, null, 2)}
-      </pre>
-    </main>
-  );
+export default function Home() {
+  redirect("/login");
 }
